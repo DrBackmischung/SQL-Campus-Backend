73af8492fb535fffdfffc7d7cd22e207115c3e48
Mathis Neunzig <mathis.neunzig@gmail.com>
2024-05-19 15:28:55 +0200

    employee

--- src/main/java/de/sqlcampus/backend/testdata/entities/Employee.java
+++ src/main/java/de/sqlcampus/backend/testdata/entities/Employee.java
@@ -1,13 +1,17 @@
 package de.sqlcampus.backend.testdata.entities;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
 public class Employee {
 
-    @Field("name")
-	private String name;
+    @Field("firstname")
+	private String firstname;
+
+    @Field("lastname")
+	private String lastname;
 
     @Field("address")
 	private String address;
@@ -23,5 +27,15 @@ public class Employee {
 
     @Field("email")
 	private String email;
+
+    @Field("salary")
+	private int salary;
+
+    @Field("role")
+	private String role;
+
+    @Field("salary")
+    @DBRef
+	private Supermarket supermarket;
 
 }
